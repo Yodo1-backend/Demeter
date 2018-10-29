ca4b0748b28872c2c2e31aeff14cd47d199481c6
yanfeng <yanfeng@yodo1.com>
2018-10-29 20:59:02 +0800

    coding;

--- example/ExampleServer.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var io = require('socket.io')(6666);
-
-io.on('connection', function (socket) {
-  socket.on('message', function () { });
-  socket.on('disconnect', function () { });
-});
